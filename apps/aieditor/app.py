1e05f566852dfa9c8d80145cb229bc6fc4c6a28e
timmyeos <97524127+timmyeos@users.noreply.github.com>
2024-01-17 15:16:47 +0900

    Update app.py
    LLM이 생성하는 답변을 stream 형식으로 화면에 표시하도록 수정.

--- apps/aieditor/app.py
+++ apps/aieditor/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, stream_template
+from flask import request, Response
 from apps.aieditor.func.chain import ScriptAssistant
 
 app = Flask(__name__)
@@ -18,17 +19,14 @@ def maintest():
     # 소주제 표시 여부를 결정하는 플래그
     display_subtopics = False
 
-    # 체크박스 상태를 저장하는 딕셔너리
-    checkbox_states = {}
-
-    # 체크박스로 선택된 스크립트를 저장할 리스트
-    selected_scripts = [0] * 10
-
     selected_model = None
 
     # POST 요청 처리
     if request.method == "POST":
-        # "소주제 표시" 버튼이 눌렸는지 확인
+        print("request:", request)
+        print("request.form:", request.form)
+
+        # "소주제 표시" 버튼이 눌린 경우, request.form으로 data가 들어옴
         if "button1" in request.form:
             user_input = request.form.get("user_input")
             display_subtopics = True if user_input.strip() else False
@@ -40,45 +38,61 @@ def maintest():
             # ScriptAssistant 인스턴스가 생성되어 있을 경우에만 make_subtopics 호출
             if script_assistant_instance:
                 subtopics = script_assistant_instance.make_subtopics(user_input)
-                print(subtopics)
+                print(subtopics)  # 한글
 
-        # "스크립트 생성" 버튼이 눌린 경우
-        if "button2" in request.form:
+        # "스크립트 생성" 버튼이 눌린 경우, request.form은 비어있고, request.json으로 data가 들어오도록 함(script.js의 fetch 참고)
+        if not (request.form) and request.json["button2"]:
             # 선택된 소주제에 대한 스크립트 생성
             display_subtopics = True
-            user_input = request.form.get("user_input")
+            user_input = request.json["user_input"]
+            checkedNames = request.json["checkedNames"]
+            print(checkedNames)
+            # checkedNames : 선택된 체크박스의 name(subtopic{i})들을 리스트로 반환(script.js의 getCheckedCheckboxNames 참고)
 
-            # ScriptAssistant 인스턴스가 생성되어 있을 경우에만 dd 호출
+            # ScriptAssistant 인스턴스가 생성되어 있을 경우에만 make_scripts 호출
             if script_assistant_instance:
-                selected_idx = [
-                    i for i in range(1, 11) if request.form.get(f"subtopic{i}")
-                ]
+                selected_idx = list(map(lambda name: int(name[8:]), checkedNames))
                 print(selected_idx)
+                # selected_idx : 선택한 subtopic의 인덱스 리스트, subtopic{i}의 숫자만 추출
+
                 selected_list = script_assistant_instance.select_subtopics(selected_idx)
-                print(selected_list)
+                print(selected_list)  # 영어 (make_scripts에 들어갈 input이라, 할당할 필요없지만 확인차 출력)
 
-                for i, idx in enumerate(selected_idx):
-                    script = script_assistant_instance.make_scripts(
-                        user_input, selected_list, str(i + 1)
-                    )
-                    # selected_scripts.append(script)
-                    selected_scripts[idx - 1] = script
-                print(selected_scripts)
+                def event_stream():
+                    """스크립트 생성을 위해 server-sent event stream을 생성합니다.
 
-        # 체크박스 상태를 업데이트
-        checkbox_states = {
-            f"subtopic{i}": "checked" if request.form.get(f"subtopic{i}") else ""
-            for i in range(1, 11)
-        }
+                    make_scripts함수가 stream으로 str chunk를 반환합니다.
+                    Gemini가 empty string을 생성하고 작동을 멈추는 경우가 있기때문에, 에러로 간주하고 종료합니다.
 
-    # 템플릿 렌더링. 변수들을 템플릿으로 전달
-    return render_template(
+                    선택된 subtopic이 n개일 때 반복문을 돌며, n개의 script를 생성합니다.
+                    각 script의 생성이 완료되면, "End of script"를 반환합니다.
+                    모든 script 생성이 완료되면, "The end"를 반환합니다.
+
+                    참고 코드: https://dev.to/jethrolarson/streaming-chatgpt-api-responses-with-python-and-javascript-22d0
+
+                    Yields:
+                        str: Chunks of generated scripts or error messages.
+                    """
+                    for i in range(len(checkedNames)):
+                        for chunk in script_assistant_instance.make_scripts(
+                            user_input, str(i + 1)
+                        ):
+                            print(chunk)
+                            if chunk:
+                                yield chunk
+                            else:
+                                yield "\n\n ⚠️ 에러가 발생했습니다! 다시 생성해주세요. ⚠️The end"
+                        yield "End of script"
+                    yield "The end"
+
+                return Response(event_stream(), mimetype="text/event-stream")
+
+    # 템플릿 스트림 렌더링. 변수들을 템플릿으로 전달
+    return stream_template(
         "maintest.html",
         subtopics=subtopics,
         user_input=user_input,
         display_subtopics=display_subtopics,
-        checkbox_states=checkbox_states,
-        selected_scripts=selected_scripts,
     )
 
 
