eb3a8033e27e161f0abf301adce5e8dcf6777e2a
yhp2205 <yhp2205@gmail.com>
2024-01-19 09:08:44 +0900

    Update music_gen.py
    music_gen이 gpu를 사용하는 코드로 수정합니다

--- apps/aieditor/func/music_gen.py
+++ apps/aieditor/func/music_gen.py
@@ -1,12 +1,16 @@
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 import scipy
+import torch
 
 # pip install git+https://github.com/huggingface/transformers.git
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"ussing device : {device}")
 
 
 processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small")
-
+model = model.to(device)
 inputs = processor(
     text=[
         "A song that uses xylophone, triangle, and guitar with a fairy-tale-like clear feel.",
@@ -16,8 +20,9 @@ inputs = processor(
     return_tensors="pt",
 )
 
+inputs = {k: v.to(device) for k, v in inputs.items()}
 audio_values = model.generate(**inputs, max_new_tokens=1500)
-
+audio_values = audio_values.to("cpu")
 
 sampling_rate = model.config.audio_encoder.sampling_rate
 scipy.io.wavfile.write(
